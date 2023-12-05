b25b7b01a7bb7210d6cc3b3af06320e5ae3aba85
Anton Smirnov <sandfox+git@sandfox.me>
2023-12-05 06:06:33 +0200

    Declare types

--- src/PathUtils.php
+++ src/PathUtils.php
@@ -6,13 +6,10 @@ namespace Arokettu\Path;
 
 final class PathUtils
 {
-    /**
-     * @param string|PathInterface $basePath
-     * @param string|PathInterface $relativePath
-     * @return string
-     */
-    public static function resolveRelativePath($basePath, $relativePath): string
-    {
+    public static function resolveRelativePath(
+        string|PathInterface $basePath,
+        string|PathInterface $relativePath,
+    ): string {
         if (\is_string($basePath)) {
             $basePath = PathFactory::parse($basePath);
         }
@@ -21,44 +18,25 @@ final class PathUtils
             $relativePath = PathFactory::parse($relativePath);
         }
 
-        if (!($basePath instanceof PathInterface)) {
-            throw new \InvalidArgumentException('basePath must be a string or an instance of PathInterface');
-        }
-
         if ($relativePath instanceof RelativePathInterface) {
             return $basePath->resolveRelative($relativePath)->toString();
         } elseif ($relativePath instanceof AbsolutePathInterface) {
             return $relativePath->toString();
         }
 
-        throw new \InvalidArgumentException('relativePath must be a string or an instance of PathInterface');
+        throw new \LogicException('PathInterface object must be either AbsolutePathInterface or RelativePathInterface');
     }
 
-    /**
-     * @param string|AbsolutePathInterface $basePath
-     * @param string|AbsolutePathInterface $targetPath
-     * @return string
-     */
-    public static function makeRelativePath($basePath, $targetPath): string
-    {
+    public static function makeRelativePath(
+        string|AbsolutePathInterface $basePath,
+        string|AbsolutePathInterface $targetPath,
+    ): string {
         if (\is_string($basePath)) {
             $basePath = PathFactory::parse($basePath);
         }
 
         if (\is_string($targetPath)) {
             $targetPath = PathFactory::parse($targetPath);
-        }
-
-        if (!($basePath instanceof AbsolutePathInterface) || !$basePath->isAbsolute()) {
-            throw new \InvalidArgumentException(
-                'basePath must be a string containing absolute path or an instance of AbsolutePathInterface'
-            );
-        }
-
-        if (!($targetPath instanceof AbsolutePathInterface) || !$targetPath->isAbsolute()) {
-            throw new \InvalidArgumentException(
-                'targetPath must be a string containing absolute path or an instance of AbsolutePathInterface'
-            );
         }
 
         return $basePath->makeRelative($targetPath)->toString();
